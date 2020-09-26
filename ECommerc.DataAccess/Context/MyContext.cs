7d457342e5247c13a3c6081c6fffbfab28a48a55
ismail.sava <ismail.sava@makrogem.com.tr>
2020-09-26 11:09:04 +0300

    Add project files.

---
+++ ECommerc.DataAccess/Context/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerc.DataAccess.Context
+{
+    public class MyContext : DbContext
+    {
+        public static string ConnectionString { get; set; }
+        public MyContext()
+        {
+        }
+
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ECommerc.Entites/Concrete/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ECommerc.Entites.Concrete
+{
+    public class Products 
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        //public virtual Company Company { get; set; }
+    }
+}
